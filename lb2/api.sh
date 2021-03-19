1a8418488e99d142a0a22436e54e07b9ef036aaf
sorry <flurin.laim@gmail.com>
2021-03-19 15:32:38 +0100

    fixed api

--- lb2/api.sh
+++ lb2/api.sh
@@ -1,9 +1,8 @@
 sudo apt-get update
 sudo apt install python3-pip -y
-sudo apt install python-pip -y
-
 
 pip3 install flask_restful
 pip3 install flask
-chmod +x /dev/main.py
-nohup /dev/main.py &
+
+sudo chmod +x /home/vagrant/main.py
+sudo nohup /home/vagrant/main.py &
